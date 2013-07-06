be631900fac05d8035dd54e27d51803ebfa604ac
Alex Bahouth <abahouth@onekingslane.com>
2013-07-05 20:27:33 -0700

    Set version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject avro-schema-diff "0.1.0-SNAPSHOT"
-  :description "A Clojure library designed to diff any two avro schemas and provide a summary of the differences."
+(defproject avro-schema-diff "0.1.0"
+  :description "A Clojure library designed to diff any two Avro Record Schemas and provide a summary of the differences."
   :url "https://github.com/Roxxi/avro-schema-diff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
