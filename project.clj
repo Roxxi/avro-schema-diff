f09a3e2b37b45dfa249660d26e0f6deda1b1456c
Matt Halverson <mhalverson@onekingslane.com>
2014-02-04 13:38:34 -0800

    Update project.clj to be okl-centric

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject roxxi/avro-schema-diff "0.1.0"
+(defproject com.onekingslane.danger/avro-schema-diff "0.1.0"
   :description "A Clojure library designed to diff any two Avro Record Schemas and provide a summary of the differences."
-  :url "https://github.com/Roxxi/avro-schema-diff"
+  :url "https://github.com/okl/danger-avro-schema-diff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.apache.avro/avro "1.7.4"]
-                 [roxxi/clojure-common-utils "0.0.11"]
+                 [com.onekingslane.danger/clojure-common-utils "0.0.19"]
                  [cheshire "5.2.0"]]
   :warn-on-reflection true)
